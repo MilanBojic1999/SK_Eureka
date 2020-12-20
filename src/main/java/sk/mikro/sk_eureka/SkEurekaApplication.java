ef3ccb872d932e84cf60e102759b8d7e5b7f4af8
MilanBojic1999 <mbojic12@gmail.com>
2020-12-20 01:08:44 +0100

    Initial commit

---
+++ src/main/java/sk/mikro/sk_eureka/SkEurekaApplication.java
@@ -0,0 +1,15 @@
+package sk.mikro.sk_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SkEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/mikro/sk_eureka/SkEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.mikro.sk_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
